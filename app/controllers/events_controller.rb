6005a19413d72dec1c40caf554e8e49aa281d023
Christopher Brum <31198605+ChristopherBrum@users.noreply.github.com>
2024-07-31 14:54:40 -0700

    created resources and associations for Category and Categorization between Category and Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,6 +12,7 @@ class EventsController < ApplicationController # rubocop:disable Style/Documenta
   def show
     @event = Event.find(params[:id])
     @likers = @event.likers
+    @categories = @event.categories
     if current_user
       @like = current_user.likes.find_by(event_id: @event.id)
     end
